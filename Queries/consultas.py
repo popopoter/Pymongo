c8d5cc919e09fe0492773d89cbf22a9e830a8121
danielwikstrom <daniel.wikstrom@live.u-tad.com>
2017-03-22 23:09:04 +0100

    queries arregladas

--- Queries/consultas.py
+++ Queries/consultas.py
@@ -3,23 +3,22 @@ from Conexion import connection
 conexion= connection.Conection()
 cliente = conexion.conected()
 def query1(comunidadAutonoma):
-    consulta = cliente.city.aggregate([{'$match': {'autonomous_community':comunidadAutonoma}},{'$project' :{'name':1,'_id':0}}])
-    for document in consulta:
-        print document
+    consulta = [{'$match': {'autonomous_community':comunidadAutonoma}},{'$project' :{'name':1,'_id':0}}]
+    return consulta
 def query2(comunidadAutonoma):
-    consulta = cliente.city.aggregate([{'$group':{
+    consulta = [{'$group':{
     '_id':comunidadAutonoma,
-    'avgElevation':{'$avg':'$elevation'}}}])
-    for document in consulta:
-        print document
+    'avgElevation':{'$avg':'$elevation'}}}]
+    
+    return consulta
 def query3(comunidadAutonoma):
-    consulta = cliente.city.aggregate([{'$group':{
+    consulta = [{'$group':{
     '_id':comunidadAutonoma,
-    'avgDensity':{'$avg':{'$divide':['$population','$area']}}}}])
-    for document in consulta:
-        print document
+    'avgDensity':{'$avg':{'$divide':['$population','$area']}}}}]
+    
+    return consulta
 def query4(provincia):
-    consulta = cliente.city.aggregate([
+    consulta = [
     {'$match':{'name':provincia}},
     {
         '$lookup':
@@ -34,11 +33,11 @@ def query4(provincia):
     {'$group':{'_id':'$poi.kind'}},
     {'$unwind':'$_id'},
     {'$group':{'_id':'$_id','count':{'$sum':1}}}
-])
-    for document in consulta:
-        print document
+]
+    
+    return consulta
 def query5(provincia):
-    consulta = cliente.city.aggregate([
+    consulta = [
     {'$match':{'name':provincia}},
     {
         '$lookup':
@@ -55,43 +54,43 @@ def query5(provincia):
     {'$group':{'_id':'$_id'}}
 
 
-])
-    for document in consulta:
-        print document
+]
+    
+    return consulta
 def query6(provincia,tipoPOI):
-    consulta = cliente.city.aggregate([
+    consulta = [
 {'$lookup':{'from':'POI','localField':'_id','foreignField':'city_id','as': 'poi'}},
 {'$match':{'name':provincia}},
 {'$unwind':'$poi'},
 {'$match':{ 'poi.kind':tipoPOI}},
 {'$group':{'_id':'$name','avgPrice':{'$avg':'$poi.avg_price'},'minPrice':{'$min':'$poi.avg_price'},'maxPrice':{'$max':'$poi.avg_price'}}},
-])
-
-    for document in consulta:
-        print document
+]
+    
+    return consulta
 def query7(tipoPOI,CoordenadaX, CoordenadaY):
-    consulta = cliente.city.aggregate([
+    consulta = [
         {"$geoNear":
              {"near": { "type": "Point", "coordinates": [ CoordenadaX,CoordenadaY] }, "distanceField": "dist.calculated", "spherical": "true"}},
         {'$lookup':{'from':'POI','localField': '_id','foreignField':'city_id','as': 'cityPOI'}},
     {"$unwind":"$cityPOI"},{"$project":{"_id":0,"cityPOI.name":1,"name":1,"cityPOI.kind":1}},
-    {'$match':{"cityPOI.kind":"Restaurant"}}])
+    {'$match':{"cityPOI.kind":"Restaurant"}}]
 
-    for document in consulta:
-        print document
+    
+    return consulta
 def query8(tipoPOI,CoordenadaX, CoordenadaY,radio):
-    consulta = cliente.POI.aggregate([
+    consulta = [
     {'$lookup':{'from':"city",'localField': "city_id",'foreignField':"_id",'as': "cityPOI"}},
     {'$project':{"_id":0,"name":1,"kind":1,"cityPOI.name":1,"cityPOI.location":1}},
     {'$match':{"kind":tipoPOI}},
     {'$match':{"cityPOI.location":{"$geoWithin": {"$centerSphere": [[CoordenadaX,CoordenadaY],radio]}}}},
     {'$project': {"name":1,"kind":1,"citiPOI.location":1}},
-    {'$limit': 5}])
+    {'$limit': 5}]
 
-    for document in consulta:
-        print document
+    
+    return consulta
+
 def query9(tipoPOI):
-    consulta = cliente.POI.aggregate([
+    consulta = [
     {'$match':{'kind':tipoPOI}},
 	{'$lookup':
             {
@@ -103,27 +102,23 @@ def query9(tipoPOI):
     {'$group': {'_id': '$name','cities':{'$addToSet':'$city.name'}}},
 	{'$project' : {'_id':1,'cities':1,'size_of_cities':{'$size':'$cities'}}},
 	{'$match': { 'size_of_cities' :{'$gt':1}}}
-])
-
-    for document in consulta:
-        print document
+]
+    return consulta
 def query10():
-    consulta = cliente.POI.aggregate([
+    consulta = [
     {'$sort': {'score': -1}},
     {'$lookup': { 'from':'city',  'localField':'city_id','foreignField':'_id', 'as': 'city'}},
     {'$group': {'_id': '$city.name', 'POI': {'$push':{'name' :'$name', 'score':'$score'}}}},
     {'$project': {'city':'$_id','_id':0,"bestPOIs": { '$slice': [ "$POI", 3]}}},
-	 ])
-
-    for document in consulta:
-        print document
+    {'$out':'POIoftheyear'}
+	 ]
+    
+    return consulta
 def query11(tipoPOI):
-    consulta = cliente.POI.aggregate([
+    consulta = [
     {'$match':{'kind':tipoPOI}},
     {'$group': {'_id': '$name','count':{'$sum':1}}},
     {'$sort':{'count':1}},
     {'$limit': 5}
-])
-
-    for document in consulta:
-        print document
+]
+    return consulta
--- objects/city.py
+++ objects/city.py
@@ -21,9 +21,14 @@ class city(object):
             cliente.city.insert(self.content)
 
         else:
-            cliente.city.update_one({'_id':self.id},{'$set':self.changed})
+            cliente.city.update({'_id':self.id} , {'$set':{ 'name':'Guipuzkua'}})
             self.changed=[]
-        return {k:v for k,v in self.content.iteritems() if(k in self.changed)}
+
+    """{k:v for k,v in self.content.iteritems() if(k in self.changed)}"""
     def query(self,number):
         pass
+dict={ "_id" : 'ObjectId("58cf0b76a586f62674226aca")', "name" : "Guipuzkua",  "autonomous_community": "Pais Vasco" }
+ciudad=city(**dict)
 
+
+ciudad.save();
